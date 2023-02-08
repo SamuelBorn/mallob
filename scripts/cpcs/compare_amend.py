70a8696584605d7d407326b27598bdff413c1878
Samuel Born <samuelborn@outlook.de>
2023-02-08 21:37:26 +0100

    Improve compare amned script

--- scripts/cpcs/compare_amend.py
+++ scripts/cpcs/compare_amend.py
@@ -1,62 +1,48 @@
 import argparse
 import json
 import os
-import time
 import diversify_amend
 import numpy as np
 
 
-def main(num_vars, num_jobs, min_overlap, step_size, num_tests):
+def main(num_vars, num_jobs, min_overlap, step_size, num_tests, num_cores):
     os.makedirs(f"scripts/cpcs/temp", exist_ok=True)
-    num_cores = 4
-
     results_group = {}
     results_nogroup = {}
 
-    for o in np.arange(min_overlap, 1, step_size):
-        results_group[o] = []
-        results_nogroup[o] = []
+    for overlap in np.arange(min_overlap, 1.0001, step_size):
+        results_group[overlap] = []
+        results_nogroup[overlap] = []
+
         for i in range(num_tests):
             instances = f"scripts/cpcs/temp/instances"
             with open(instances, "w") as f:
-                f.writelines(line + "\n" for line in diversify_amend.main(num_vars, o, num_jobs))
+                f.writelines(line + "\n" for line in diversify_amend.main(num_vars, overlap, num_jobs))
 
-            print(f"Nogroup {i}/{num_tests}")
-            start = time.time()
-            os.system(f'mpirun -np {num_cores} --bind-to core build/mallob -v=4 -c=1 -ajpc={num_jobs} -ljpc={2 * num_jobs} -J={num_jobs} \
-                        -job-desc-template={instances} \
-                        -job-template=scripts/cpcs/input/job-nogroup.json \
-                        -client-template=templates/client-template.json -pls=0 \
-                        | grep -i "cpcs\|exited\|error\|warn\|solution\|over-due\|RESPONSE_TIME\|idle" \
-                        && pkill mallob')
-            end = time.time()
-            print(end - start)
-            results_group[o].append(end - start)
+            for identifier in ["nogroup", "group"]:
+                print(f"{identifier} {i}/{num_tests}")
+                output = os.popen(f'mpirun -np {num_cores} --bind-to core build/mallob -v=2 -c=1 -ajpc={num_jobs} -ljpc={2 * num_jobs} -J={num_jobs} \
+                                    -job-desc-template={instances} \
+                                    -job-template=scripts/cpcs/input/job-{identifier}.json \
+                                    -client-template=templates/client-template.json').read()
+                filtered = [float(line.split(" ")[4]) for line in output.splitlines() if "RESPONSE_TIME" in line]
+                results_nogroup[overlap].append(sum(filtered))
+                print(sum(filtered))
 
-            print(f"Group {i}/{num_tests}")
-            start = time.time()
-            os.system(f'mpirun -np {num_cores} --bind-to core build/mallob -v=4 -c=1 -ajpc={num_jobs} -ljpc={2 * num_jobs} -J={num_jobs} \
-                        -job-desc-template={instances} \
-                        -job-template=scripts/cpcs/input/job-group-check.json \
-                        -client-template=templates/client-template.json -pls=0 \
-                        | grep -i "cpcs\|exited\|error\|warn\|solution\|over-due\|RESPONSE_TIME\|idle" \
-                        && pkill mallob')
-            end = time.time()
-            print(end - start)
-            results_group[o].append(end - start)
-
-            with open("scripts/cpcs/output/group.json", "w") as f:
-                f.write(json.dumps(results_group))
-            with open("scripts/cpcs/output/nogroup.json", "w") as f:
-                f.write(json.dumps(results_nogroup))
+                with open("scripts/cpcs/output/{identifier}.json", "w") as f:
+                    if identifier == "group":
+                        f.write(json.dumps(results_group))
+                    else:
+                        f.write(json.dumps(results_nogroup))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", help="Num vars in problem", type=int, default=1700)
+    parser.add_argument("-v", help="Num vars in problem", type=int, default=1000)
     parser.add_argument("-j", help="Num jobs to run", type=int, default=3)
-    parser.add_argument("-m", help="Min overlap between problems", type=float, default=0.9)
+    parser.add_argument("-m", help="Min overlap between problems", type=float, default=0.97)
     parser.add_argument("-s", help="Step size of overlap", type=float, default=0.01)
     parser.add_argument("-n", help="Tests to perform", type=int, default=10)
+    parser.add_argument("-c", help="Num cores to execute on", type=int, default=16)
     args = parser.parse_args()
-    main(args.v, args.j, args.m, args.s, args.n)
+    main(args.v, args.j, args.m, args.s, args.n, args.c)
