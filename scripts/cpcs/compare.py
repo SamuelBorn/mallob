0ccd89e35df737b8d307fa9d7b1f52cbfb6427ff
Samuel Born <samuelborn@outlook.de>
2023-02-27 11:11:36 +0100

    Clean up Scripts

--- scripts/cpcs/compare.py
+++ scripts/cpcs/compare.py
@@ -12,13 +12,10 @@ def main(single_problem, instance_file, output_file_path, n, j, num_cores, timeo
         with open(instance_file, "w") as f:
             for _ in range(j):
                 f.write(single_problem + "\n")
-    else:
-        with open(instance_file, "r") as f:
-            j = len(f.readlines())
 
     results = {
-        'nogroup': run_multiple(timeout, instance_file, j, num_cores, 'nogroup', n),
-        'group-nocheck': run_multiple(timeout, instance_file, j, num_cores, 'group-nocheck', n)
+        'nogroup': run_multiple_unfiltered(timeout, instance_file, j, num_cores, 'nogroup', n),
+        'group-check': run_multiple_unfiltered(timeout, instance_file, j, num_cores, 'group-check', n)
     }
 
     with open(output_file_path, "w") as f:
@@ -48,6 +45,30 @@ def run_multiple(timeout_seconds, instance_file, num_jobs, num_cores, identifier
     return results
 
 
+def run_once_unfiltered(timeout_seconds, instance_file, num_jobs, num_cores, identifier_group_nogroup, stop_after=None):
+    if not stop_after: stop_after = num_jobs
+
+    output = subprocess.check_output(f'mpirun -np {num_cores} --bind-to core build/mallob '
+                                     f'-jwl={timeout_seconds} -v=2 -c=1 -ajpc={num_jobs} -ljpc={2 * num_jobs} -J={stop_after} '
+                                     f'-job-desc-template={instance_file} '
+                                     f'-job-template=scripts/cpcs/input/job-{identifier_group_nogroup}.json '
+                                     f'-client-template=templates/client-template.json', shell=True)
+
+    filtered = [float(line.split(" ")[4]) for line in output.decode("utf-8").split("\n") if "RESPONSE_TIME" in line]
+    while len(filtered) < stop_after: filtered.append(2 * timeout_seconds)
+    return filtered
+
+
+def run_multiple_unfiltered(timeout_seconds, instance_file, num_jobs, num_cores, identifier_group_nogroup, n, stop_after=None):
+    results = []
+    for i in range(n):
+        print(f"{datetime.datetime.now()} - {i}/{n} - {identifier_group_nogroup}")
+        result = run_once_unfiltered(timeout_seconds, instance_file, num_jobs, num_cores, identifier_group_nogroup, stop_after)
+        print(result)
+        results.append(result)
+    return results
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", help="Input a single Problem file (will get duplicated)")
@@ -56,6 +77,6 @@ if __name__ == '__main__':
     parser.add_argument("-n", help="Tests to perform", type=int, default=20)
     parser.add_argument("-j", help="Num jobs to run with", type=int, default=2)
     parser.add_argument("-c", help="Num cores to execute on", type=int, default=4)
-    parser.add_argument("-t", help="Timeout in seconds", type=int, default=240)
+    parser.add_argument("-t", help="Timeout in seconds", type=int, default=1000)
     args = parser.parse_args()
     main(args.p, args.i, args.o, args.n, args.j, args.c, args.t)
--- scripts/cpcs/visualize.py
+++ scripts/cpcs/visualize.py
@@ -10,6 +10,10 @@ import sys
 
 
 def main():
+    visualize_unfiltered()
+
+
+def visualize_filtered():
     plt.rc('font', size=18)
     plt.figure(figsize=(8, 14))
     plt.ylabel("Execution time (seconds)")
@@ -22,6 +26,22 @@ def main():
             plt.boxplot(data.values(), labels=[key for key in data.keys()], medianprops=dict(linewidth=3, color='green'), boxprops=dict(color='blue'))
         else:
             plt.boxplot(data.values(), labels=[key for key in data.keys()], medianprops=dict(linewidth=3, color='orange'), boxprops=dict(color='red'))
+    plt.show()
+
+
+def visualize_unfiltered():
+    plt.rc('font', size=18)
+    plt.figure(figsize=(8, 14))
+    plt.ylabel("Execution time (seconds)")
+
+    for file_idx, file in enumerate(sys.argv[1:]):
+        with open(file) as f:
+            data = json.load(f)
+
+        if file_idx == 0:
+            plt.boxplot(sum(data.values()), labels=[key for key in data.keys()], medianprops=dict(linewidth=3, color='green'), boxprops=dict(color='blue'))
+        else:
+            plt.boxplot(sum(data.values()), labels=[key for key in data.keys()], medianprops=dict(linewidth=3, color='orange'), boxprops=dict(color='red'))
     plt.show()
 
 
